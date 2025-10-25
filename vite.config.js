bb5f95888a64a9ad37218dd0d1b3dbd4e65db7e3
anhaylientuc <thang3051998no1@gmail.com>
2025-10-26 00:19:15 +0700

    ok

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: './',          // rất quan trọng khi deploy tĩnh
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main:   resolve(__dirname, 'index.html'),
+        online: resolve(__dirname, 'online.html'), // ép build thêm trang này
+      },
+    },
+  },
+});
